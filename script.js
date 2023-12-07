86590c7ef6c0df211aec0c709a4d2de9a4b18d0e
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-12-07 11:41:42 +0200

    add basic structure and math logic in js

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function operate(number1, operator, number2) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+
+    case "*":
+      return multiply(number1, number2);
+
+    case "/":
+      return divide(number1, number2);
+  }
+}
